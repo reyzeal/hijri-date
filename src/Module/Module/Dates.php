64ecf602ae24981e17b7c35b62d1d27f173f8f8b
Rizal Ardhi Rahmadani <reyzeal@users.noreply.github.com>
2017-01-29 18:13:31 +0700

    Delete Dates.php

--- src/Module/Module/Dates.php
+++
@@ -1,9 +0,0 @@
-<?PHP
-namespace Reyzeal\HijriTimeBot\Modules;
-use Reyzeal\HijriTimeBot\Foundation\Connection as Connection;
-class Dates extends Module{
-	protected $date;
-	public function execute(){
-		return (new Connection)->fetch($this->module.$this->date);
-	}
-}
